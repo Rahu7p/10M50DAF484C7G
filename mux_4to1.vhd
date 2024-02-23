9582e6f944dd2f45ae1ed41f75f8b9d3dccf11de
Rahu7p <78507802+Rahu7p@users.noreply.github.com>
2024-02-23 17:42:19 -0600

    Update mux_4to1.vhd

--- mux_4to1.vhd
+++ mux_4to1.vhd
@@ -1,18 +1,18 @@
-LIBRARY  ieee;
-USE      ieee.std_logic_1164.ALL;
+LIBRARY	ieee;
+USE	ieee.std_logic_1164.ALL;
 
 ENTITY mux_4to1 IS
-  PORT(
-    x0, x1, x2, x3  :  IN   std_logic;
-    sel             :  IN   std_logic_vector( 1 DOWNTO 0 );
-    y               :  OUT  std_logic 
-  );
+	PORT(
+    		x0, x1, x2, x3	:  IN	std_logic;
+    		sel		:  IN	std_logic_vector( 1 DOWNTO 0 );
+   		y		:  OUT	std_logic 
+	);
 END ENTITY;
 
 ARCHITECTURE dataflow OF mux_4to1 IS
 BEGIN
-  y <=  x3 WHEN sel = "11" ELSE
-        x2 WHEN sel = "10" ELSE
-	x1 WHEN sel = "01" ELSE
-	x0;
+	y <=	x3 WHEN sel = "11" ELSE
+        	x2 WHEN sel = "10" ELSE
+		x1 WHEN sel = "01" ELSE
+		x0;
 END ARCHITECTURE;
