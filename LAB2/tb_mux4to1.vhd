bbde0dd1e7a19e9a9096741d709f22158c769b8c
Rahu7p <78507802+Rahu7p@users.noreply.github.com>
2024-02-28 04:46:18 -0600

    Update tb_mux4to1.vhd

--- LAB2/tb_mux4to1.vhd
+++ LAB2/tb_mux4to1.vhd
@@ -1,8 +1,5 @@
 LIBRARY ieee;
 USE	ieee.std_logic_1164.ALL;
-USE	ieee.std_logic_signed.ALL;
-USE	ieee.std_logic_textio.ALL;
-USE	std.textio.ALL;
 
 --Entity: no port list!
 ENTITY  tb_mux_4to1 IS
@@ -40,21 +37,4 @@ BEGIN
 						x0_tb <= '0'; x1_tb <= '1'; x2_tb <= '0'; x3_tb <= '1'; sel_tb <= "11"; expect <= '1';
 					WAIT;
 				END PROCESS;
-						
-	--Monitor
-	txt_out		:	PROCESS( y_tb )
-				VARIABLE str_o	:	LINE;
-			  		BEGIN
-					WRITE( str_o, STRING'( " x0= " 		));	WRITE( str_o, x0_tb 	);
-					WRITE( str_o, STRING'( " x1= " 		));	WRITE( str_o, x1_tb 	);
-					WRITE( str_o, STRING'( " x2= " 		));	WRITE( str_o, x2_tb 	);
-					WRITE( str_o, STRING'( " x3= " 		));	WRITE( str_o, x3_tb 	);
-					WRITE( str_o, STRING'( " sel= " 	));	WRITE( str_o, sel_tb  	);
-					WRITE( str_o, STRING'( " y= " 		));	WRITE( str_o, y_tb 	);
-					WRITE( str_o, STRING'( " expect= "  	));	WRITE( str_o, expect	);
-					ASSERT false REPORT TIME'IMAGE( NOW ) & str_o.ALL
-						SEVERITY note;
-					DEALLOCATE( str_o );
-				END PROCESS;		
-
 END ARCHITECTURE;
