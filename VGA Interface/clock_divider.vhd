3a1379812389a2318e571f78b4da243a32fc8692
Rahu7p <78507802+Rahu7p@users.noreply.github.com>
2024-04-08 11:33:59 -0600

    Update clock_divider.vhd

--- VGA Interface/clock_divider.vhd
+++ VGA Interface/clock_divider.vhd
@@ -5,34 +5,28 @@ USE 		ieee.std_logic_unsigned.all;
 ENTITY clock_divider IS
    PORT( 
 		clk, rst	:	IN  STD_LOGIC;
-      		clk_out	: 	OUT STD_LOGIC 
+      		clk_out		: 	OUT STD_LOGIC 
 	);
 END clock_divider;
 
 
 ARCHITECTURE behavior OF clock_divider IS
-
 	SIGNAL count : POSITIVE;
 	SIGNAL tmp   : STD_LOGIC := '0';
-
-	
-
 BEGIN
-   	
-		PROCESS( clk, rst )
-			BEGIN
-      				IF rst = '0' THEN 
-					count <= 1; 
-					tmp   <= '0';
-				ELSIF rising_edge( clk ) THEN
-					count <= count + 1;
-					-- ( 50MHz/1Hz ) * 0.5 
-					IF( count = 25000000 ) THEN
-						tmp   <= NOT tmp;
-						count <= 1;
-					END IF;
-				END IF;
-				clk_out <= tmp;
-		END PROCESS;
-
+	PROCESS( clk, rst )
+	BEGIN
+		IF rst = '0' THEN 
+			count <= 1; 
+			tmp   <= '0';
+		ELSIF rising_edge( clk ) THEN
+			count <= count + 1;
+			-- ( 50MHz/1Hz ) * 0.5 
+			IF( count = 25000000 ) THEN
+				tmp   <= NOT tmp;
+				count <= 1;
+			END IF;
+		END IF;
+		clk_out <= tmp;
+	END PROCESS;
 END behavior;
