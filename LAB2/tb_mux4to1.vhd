16c3d3146950d918e1d7cdc875aa54898ecf88ed
Raul <78507802+Rahu7p@users.noreply.github.com>
2026-02-28 08:34:48 -0600

    Update tb_mux4to1.vhd

--- LAB2/tb_mux4to1.vhd
+++ LAB2/tb_mux4to1.vhd
@@ -6,36 +6,39 @@ ENTITY  tb_mux_4to1 IS
 END	tb_mux_4to1;
 
 --Architecture
-ARCHITECTURE test_arch OF tb_mux_4to1 IS
+ARCHITECTURE sim OF tb_mux_4to1 IS
 	COMPONENT mux_4to1
-		PORT(
-			x0, x1, x2, x3	:	IN	STD_LOGIC;
-			sel		:	IN	STD_LOGIC_VECTOR( 1 DOWNTO 0 );
-			y		:	OUT	STD_LOGIC
-		);
+		PORT( 
+			x0, x1, x2, x3	: IN  std_logic; 	
+			sel 			: IN  std_logic_vector( 1 DOWNTO 0 );
+          	y 				: OUT std_logic );
 	END COMPONENT;
-	
-SIGNAL	x0_tb, x1_tb, x2_tb, x3_tb	:	STD_LOGIC;			--INPUT
-SIGNAL	sel_tb				:	STD_LOGIC_VECTOR( 1 DOWNTO 0 );	--INPUT
-SIGNAL	y_tb				:	STD_LOGIC;			--OUTPUT
-SIGNAL	expect				:	STD_LOGIC;			--expected
+
+SIGNAL x0, x1, x2, x3 	: 	std_logic; 						--INPUT 
+SIGNAL sel 				:	std_logic_vector( 1 DOWNTO 0 ); --INPUT
+SIGNAL y 				:	std_logic; 						--OUTPUT
+SIGNAL expect 			:	std_logic; 						--expected OUTPUT
 
 BEGIN
-	--DUT Instantiation
-	DUT		:	mux_4to1 PORT MAP( x0_tb, x1_tb, x2_tb, x3_tb, sel_tb, y_tb );
-	
-	--Stimulus by hand drawn waves, poor coverage
-	stim_proc	:	PROCESS
-			  		BEGIN
-					WAIT FOR 0 ns;
-						x0_tb <= '0'; x1_tb <= '1'; x2_tb <= '0'; x3_tb <= '1'; sel_tb <= "00"; expect <= '0';
-					WAIT FOR 10 ns;
-						x0_tb <= '0'; x1_tb <= '1'; x2_tb <= '0'; x3_tb <= '1'; sel_tb <= "01"; expect <= '1';
-					WAIT FOR 10 ns;
-						x0_tb <= '0'; x1_tb <= '1'; x2_tb <= '0'; x3_tb <= '1'; sel_tb <= "10"; expect <= '0';
-					WAIT FOR 10 ns;
-						x0_tb <= '0'; x1_tb <= '1'; x2_tb <= '0'; x3_tb <= '1'; sel_tb <= "11"; expect <= '1';
-					WAIT FOR 10 ns;
-					WAIT;
-				END PROCESS;
-END ARCHITECTURE;
+	--DUT instantiation
+	DUT : mux_4to1 PORT MAP( x0 => x0, x1 => x1, x2 => x2, x3 => x3, sel => sel, y => y );
+	--Stimulus, apply inputs one at a time
+	PROCESS 
+	BEGIN
+    		x0 <= '0'; x1 <= '1'; x2 <= '0'; x3 <= '1'; 
+    		sel <= "00"; expect <= '0';
+    	WAIT FOR 10 ns;
+      	ASSERT( y = expect ) REPORT "Mismatch! when sel=0" SEVERITY error; --verify after the WAIT
+      		sel <= "01"; expect <= '1';
+    	WAIT FOR 10 ns;
+      	ASSERT( y = expect ) REPORT "Mismatch! when sel=1" SEVERITY error;      
+      		sel <= "10"; expect <= '0';
+    	WAIT FOR 10 ns;
+      	ASSERT( y = expect ) REPORT "Mismatch! when sel=2" SEVERITY error;
+      		sel <= "11"; expect <= '1';
+    	WAIT FOR 10 ns;
+      	ASSERT( y = expect ) REPORT "Mismatch! when sel=3" SEVERITY error;    
+		WAIT; --wait forever!!
+  	END PROCESS;
+END sim;
+
