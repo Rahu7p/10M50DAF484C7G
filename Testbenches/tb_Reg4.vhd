e49a95a26cbd6936ec5853ec9d662631e2fdf366
Raul <78507802+Rahu7p@users.noreply.github.com>
2026-03-09 11:01:40 -0600

    Refactor VHDL testbench formatting and assertions

--- Testbenches/tb_Reg4.vhd
+++ Testbenches/tb_Reg4.vhd
@@ -1,13 +1,12 @@
 LIBRARY ieee;
-USE		  ieee.std_logic_1164.ALL;
-USE		  ieee.numeric_std.ALL;
+USE		ieee.std_logic_1164.ALL;
+USE		ieee.numeric_std.ALL;
 
-ENTITY tb_Reg4 IS
-END	   tb_Reg4;
+ENTITY 	tb_Reg4 IS
+END	 	tb_Reg4;
 
 ARCHITECTURE testbench OF tb_Reg4 IS
-	
-  COMPONENT Reg4 IS
+	COMPONENT Reg4 IS
 		PORT(
 			clk, rst, ld	: IN  std_logic;
 			I				: IN  std_logic_vector( 3 downto 0 );
@@ -28,7 +27,7 @@ ARCHITECTURE testbench OF tb_Reg4 IS
 			WHILE stop_clk = '0' LOOP
 				clk <= '0';
             WAIT FOR 5 ns;
-            	clk <= '1';
+            clk <= '1';
             WAIT FOR 5 ns;
 			END LOOP;
 			WAIT;  -- stop the process
@@ -53,7 +52,11 @@ ARCHITECTURE testbench OF tb_Reg4 IS
 			ASSERT( Q = expect ) REPORT "Failed for I = "& integer'IMAGE( to_integer(unsigned(I)) ) SEVERITY error;
 				ld <= '1';
 				I  <= "1001";
-				expect <= "0011";
+			WAIT FOR 7 ns;
+				rst <= '0';
+				expect <= "0000";
+			WAIT FOR 2 ns;
+				rst <= '1';
 			WAIT UNTIL rising_edge( clk );
 			ASSERT( Q = expect ) REPORT "Failed for I = "& integer'IMAGE( to_integer(unsigned(I)) ) SEVERITY error;
 				stop_clk <= '1';
